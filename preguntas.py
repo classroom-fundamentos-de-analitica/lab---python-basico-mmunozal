25dd57d17d95f62a4d42e47281605252ba05dc0e
mmunozal <55594437+mmunozal@users.noreply.github.com>
2022-09-04 14:40:20 -0500

    Update preguntas.py

--- preguntas.py
+++ preguntas.py
@@ -218,6 +218,16 @@ def pregunta_06():
   return ayuda
 
 
+def itemgetter(*items):
+    if len(items) == 1:
+        item = items[0]
+        def g(obj):
+            return obj[item]
+    else:
+        def g(obj):
+            return tuple(obj[item] for item in items)
+    return g
+
 def pregunta_07():
   a = open("data.csv", "r").readlines()
   a = [z.replace("\n", "") for z in a]
@@ -248,28 +258,32 @@ def pregunta_07():
 
 
 def pregunta_08():
-    """
-    Genere una lista de tuplas, donde el primer elemento de cada tupla contiene  el valor
-    de la segunda columna; la segunda parte de la tupla es una lista con las letras
-    (ordenadas y sin repetir letra) de la primera  columna que aparecen asociadas a dicho
-    valor de la segunda columna.
-
-    Rta/
-    [
-        (0, ["C"]),
-        (1, ["B", "E"]),
-        (2, ["A", "E"]),
-        (3, ["A", "B", "D", "E"]),
-        (4, ["B", "E"]),
-        (5, ["B", "C", "D", "E"]),
-        (6, ["A", "B", "C", "E"]),
-        (7, ["A", "C", "D", "E"]),
-        (8, ["A", "B", "D", "E"]),
-        (9, ["A", "B", "C", "E"]),
-    ]
-
-    """
-    return
+  a = open("data.csv", "r").readlines()
+  a = [z.replace("\n", "") for z in a]
+  a = [data.split("\t") for data in a]
+  a = [(int(data[1]),data[0]) for data in a]
+  a = sorted(a,key=itemgetter(0))
+  tuples = []
+  previous_key = None
+  acum = 0
+  i = 0
+  letras = []
+  while(True):
+    key, value = a[i]
+    if previous_key is None:
+        previous_key = key
+    if key != previous_key:
+        tuples.append((previous_key,sorted(set(letras))))
+        previous_key = key
+        letras = []
+        letras.append(value)
+    else:
+        letras.append(value)
+    i += 1
+    if i == len(a):
+        tuples.append((previous_key,sorted(set(letras))))
+        break
+  return tuples
 
 
 def pregunta_09():
